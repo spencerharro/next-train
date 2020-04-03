d18df55b202b7dc625de6544e70c91610b1fbf81
Spencer Harro <spencer.harro@gmail.com>
2020-04-03 13:22:17 -0700

    Created next train table helper functions.  TODO fix how table width isn't same size as header

--- NextTrain.js
+++ NextTrain.js
@@ -1,12 +1,14 @@
 // Populate lines dropdown with available lines when page loads
 $( document ).ready(function() {
     getLineList();
+    createNextTrainTable();
 });
 
 /** Populates table with next trains arriving at the specified station. */
 function getNextTrainList() {
     let last_train = $('#trainTable tr:last');
     let stationCode = document.getElementById("station_dropdown").value;
+    eraseNextTrainTable();
     var query = {
         "async": true,
         "crossDomain": true,
@@ -20,8 +22,8 @@ function getNextTrainList() {
     .done(function (response) {
       for (train in response.Trains) {
     		// Add list of next trains and ETA's to page (based on selected line/station)
-    		last_train.after('<tr><td>',response.Trains[train].Destination,'</td><td>',response.Trains[train].Min,'</td></tr>');
-    	}
+        insertNextTrainTable(response.Trains[train].Destination,response.Trains[train].Min)
+  		}
     });
 }
 
@@ -72,4 +74,30 @@ function getLineList() {
       lines_dropdown.append($('<option></option>').attr('value', response.Lines[index].LineCode).text(response.Lines[index].DisplayName));
     }
   })
+}
+
+/** Initializes the next train table */
+function createNextTrainTable() {
+  // Create next train table and initialize the header
+  nextTrainTbl = document.getElementById('trainTable');
+  header = nextTrainTbl.createTHead();
+  headerRow = nextTrainTbl.insertRow(0);
+  headerRow.insertCell(0).innerHTML = "<b>Destination</b>";
+  headerRow.insertCell(1).innerHTML = "<b>Departs In (Min)</b>";
+}
+
+function eraseNextTrainTable() {
+  // Erase next train table (except for header (row 0))
+  nextTrainTbl = document.getElementById('trainTable');
+  while(testTable.rows.length > 1) {
+    testTable.deleteRow(1);
+  }
+}
+
+function insertNextTrainTable(dest, time) {
+  // Insert next train destination (dest) and departs in (time) into next train table
+  nextTrainTbl = document.getElementById('trainTable');
+  row = testTable.insertRow();
+  row.insertCell(0).innerHTML = dest;
+  row.insertCell(1).innerHTML = time;
 }
